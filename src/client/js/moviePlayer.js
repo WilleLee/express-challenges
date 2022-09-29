733cbc77e709297bdbc3ae22144b2e9851fe72c1
Wille Lee <1992season@gmail.com>
2022-09-29 18:53:27 +0900

    add moviePlayer.js into webpack config and add css for movieDetail.pug

---
+++ src/client/js/moviePlayer.js
@@ -0,0 +1,6 @@
+const playBtn = document.getElementById("playBtn");
+const muteBtn = document.getElementById("muteBtn");
+const volumeControl = document.getElementById("volumeControl");
+const timelineControl = document.getElementById("timelineControl");
+
+console.log("movie player");
