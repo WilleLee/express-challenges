d6c1810842086e3fcc4735550b311481f6f470cc
Wille Lee <1992season@gmail.com>
2022-10-01 20:21:50 +0900

    add events to timelineControl

--- src/client/js/moviePlayer.js
+++ src/client/js/moviePlayer.js
@@ -3,6 +3,8 @@ const playBtn = document.getElementById("playBtn");
 const muteBtn = document.getElementById("muteBtn");
 const volumeControl = document.getElementById("volumeControl");
 const timelineControl = document.getElementById("timelineControl");
+const curTime = document.getElementById("curTime");
+const ttlTime = document.getElementById("ttlTime");
 
 video.volume = volumeControl.value;
 
@@ -52,12 +54,13 @@ const handleMuteBtn = () => {
   }
 };
 /**
- * function to receive the input value of volumeControl and adjust video.volume and muteBtn.innerText
+ * function to receive the input value of volumeControl and adjust video.volume and muteBtn.innerText.
  * @param {Object} event
  */
 const handleVolumeControl = (event) => {
   const value = Number(event.target.value);
   video.volume = value;
+  // the default value of input is string so that it must be altered to number in order to adjust muteBtn.innerText using the conditional statement below
   value > 0 ? handleVideoUnmuted() : handleVideoMuted();
 };
 
@@ -68,6 +71,31 @@ const handleVideoUnmuted = () => {
   muteBtn.innerText = "Mute";
 };
 
+const handleLoadedmetadata = () => {
+  const { duration } = video;
+  timelineControl.max = Math.floor(duration);
+  const m = Math.floor(duration / 60);
+  const s = Math.floor(duration % 60);
+  ttlTime.innerText = `${String(m).padStart(2, "0")}:${String(s).padStart(
+    2,
+    "0"
+  )}`;
+};
+const handleTimeupdate = () => {
+  const { currentTime } = video;
+  const m = Math.floor(currentTime / 60);
+  const s = Math.floor(currentTime % 60);
+  curTime.innerText = `${String(m).padStart(2, "0")}:${String(s).padStart(
+    2,
+    "0"
+  )}`;
+  timelineControl.value = currentTime;
+};
+const handleTimelineControl = (event) => {
+  const value = Number(event.target.value);
+  video.currentTime = value;
+};
+
 // play or pause the video
 playBtn.addEventListener("click", handlePlayBtn);
 video.addEventListener("play", handleVideoPlayed);
@@ -76,3 +104,7 @@ video.addEventListener("pause", handleVideoPaused);
 muteBtn.addEventListener("click", handleMuteBtn);
 //adjust video.volume by using volumeControl
 volumeControl.addEventListener("input", handleVolumeControl);
+// handle timeline
+video.addEventListener("loadedmetadata", handleLoadedmetadata);
+video.addEventListener("timeupdate", handleTimeupdate);
+timelineControl.addEventListener("input", handleTimelineControl);
